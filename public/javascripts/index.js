25184824dbde94137d77e8ec4266ea97efc63987
Baha <bvx5009@psu.edu>
2016-04-26 18:27:41 -0400

    finishing up clientportal looks part 1/2

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,10 +3,26 @@ $(document).ready(function() {
   $('#welcomeLeft, #welcomeRight').waypoint(function () {
   $('#welcomeLeft').addClass('fadeInLeft'),
   $('#welcomeRight').addClass('fadeInRight');
-}, { offset: '60%' });
+  }, { offset: '60%' });
 
   $('#mainInfoUp').waypoint(function () {
   $('#mainInfoUp').addClass('fadeInUp');
+  }, { offset: '60%' });
+
+
+  $('#contentTwoLeft, #contentTwoRight').waypoint(function () {
+  $('#contentTwoLeft').addClass('fadeInLeft'),
+  $('#contentTwoRight').addClass('fadeInRight');
+  }, { offset: '60%' });
+
+  $('#doctorLeft, #doctorRight').waypoint(function () {
+  $('#doctorLeft').addClass('fadeInLeft'),
+  $('#doctorRight').addClass('fadeInRight');
+  }, { offset: '60%' });
+
+  $('#contactLeft, #contactRight').waypoint(function () {
+  $('#contactLeft').addClass('fadeInLeft'),
+  $('#contactRight').addClass('fadeInRight');
   }, { offset: '60%' });
 
   $('#myHeader').addClass('fadeInRight');
